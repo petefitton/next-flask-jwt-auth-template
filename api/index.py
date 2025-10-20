cb619f42362798558afbc25f93ca0274ffee5ba4
Pete Fitton <petefitton@gmail.com>
2025-10-20 16:46:49 -0600

    Install flask-bcrypt, stub out /api/signup route, and update requirements.txt

--- api/index.py
+++ api/index.py
@@ -1,14 +1,16 @@
 import os
 from dotenv import load_dotenv
-from api.models import db
-from flask import Flask
+from api.models import db, User
+from flask import Flask, request
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+bcrypt = Bcrypt(app)
 db.init_app(app)
 migrate = Migrate(app, db)
 
@@ -17,10 +19,16 @@ migrate = Migrate(app, db)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/api/signup")
+@app.route("/api/signup", methods=['POST'])
 def signup():
-    # take username and password and create new User in DB
-    return "<p>Hello, World!</p>"
+    hash: bytes = bcrypt.generate_password_hash(str(request.form['password'])) # type: ignore
+    user = User(
+        username=request.form['username'],
+        pw_hash=hash
+    )
+    db.session.add(user)
+    db.session.commit()
+    return "<p>User created!</p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
